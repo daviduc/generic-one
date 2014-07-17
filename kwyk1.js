21bbfe8d95bee027998ca8397ddca715429acdf5
David Cho <daviduc1969@gmail.com>
2014-07-17 12:50:53 -0700

    july 17 2014 12:50

---
+++ kwyk1.js
@@ -0,0 +1,23 @@
+﻿
+(function ($) {
+    $.fn.html5jTabs = function (options) {
+        return this.each(function (index, value) {
+            var obj = $(this), objFirst = obj.eq(index), objNotFirst = obj.not(objFirst);
+            $("#" + objNotFirst.attr("data-toggle")).hide();
+            $(this).eq(index).addClass("active");
+            obj.click(function (evt) {
+                toggler = "#" + obj.attr("data-toggle");
+                togglerRest = $(toggler).parent().find("div");
+                togglerRest.hide().removeClass("active");
+                $(toggler).show().addClass("active");
+                $(this).parent("div").find("a").removeClass("active");
+                $(this).addClass("active");
+                if (obj.attr("data-toggle") == "jobs") {
+                    $("#sidebar").show();
+                    $("#pending_changes").show(); $(".pending_change").show();
+                }
+                return false;
+            });
+        });
+    };
+} (jQuery));
