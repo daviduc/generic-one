ef02c7c614b21b5e2fdb6f2178a1e8de032c47cc
daviduc <daviduc1969@gmail.com>
2014-07-20 17:30:13 -0700

    july 20 2014 17:30

--- web.js
+++ web.js
@@ -7,6 +7,40 @@ var app = express();
 
 var fs = require('fs');
 
+var mongo = require('mongodb');
+var mongo_client = mongo.MongoClient;
+
+mongo_client.connect(process.env.MONGOHQ_URL, function(err, db) {
+	  // operate on the collection named "test"
+	  var collection = db.collection('test')
+	 
+	  // remove all records in collection (if any)
+	  console.log('removing documents...')
+	  collection.remove(function(err, result) {
+	    if (err) {
+	      return console.error(err)
+	    }
+	    console.log('collection cleared!')
+	    // insert two documents
+	    console.log('inserting new documents...')
+	    collection.insert([{name: 'tester'}, {name: 'coder'}], function(err,
+	docs) {
+	      if (err) {
+	        return console.error(err)
+	      }
+	      console.log('just inserted ', docs.length, ' new documents!')
+	      collection.find({}).toArray(function(err, docs) {
+	        if (err) {
+	          return console.error(err)
+	        }
+	        docs.forEach(function(doc) {
+	          console.log('found document: ', doc)
+	        })
+	      })
+	    })
+	  })
+	})
+
 
 app.get('/', function(request, response) {
   console.log("TRACE1");
